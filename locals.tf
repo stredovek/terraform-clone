28ecc58410c21c94396493a41f86861fbda8b505
Petr Simek <petr.simek@kyndryl.com>
2023-07-31 12:05:40 +0200

    Initial commit of terraform code

---
+++ locals.tf
@@ -0,0 +1,25 @@
+locals {
+  resource_group_name="deletemeafter"
+  location="West Europe"
+
+ virtual_network={
+    name="test-network"
+    address_space="10.5.0.0/16"
+  }
+
+  subnets=[
+    {
+      name="Networkgateway"
+      address_prefix="10.5.0.0/24"
+    },
+    {
+      name="vda"
+      address_prefix="10.5.1.0/24"
+    },
+    {
+     name="infra"
+      address_prefix="10.5.2.0/24"   
+    }
+
+  ]
+  }
