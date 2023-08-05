b051f8aa8dad6d2acac08c367d84733ebcfa4ef0
stredovek <101272250+stredovek@users.noreply.github.com>
2023-08-05 19:35:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
  resource "azurerm_resource_group" "rg1" {
-  name     = local.resource_group_name
-  location = local.location  
+  name     = var.resource_group_name
+  location = var.location  
 }
 
 
